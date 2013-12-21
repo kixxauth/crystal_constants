5fe63451ddd06eacee692f44cf23fdd9c326c83c
Kristoffer Walker <kris@kixx.name>
2013-12-21 14:04:52 -0500

    Use Constructor factory
     On branch master
    
    	modified:   index.js
    	modified:   test/test.js

--- index.js
+++ index.js
@@ -1,39 +1,99 @@
-function Crystal() {}
+exports.newCrystalConstructor = function () {
+  var privateValues = []
 
-Crystal.prototype.define = function (name, value) {
-  if (typeof name === 'object') {
-    var self = this, values = name
-    Object.keys(name).forEach(function (key) {
-      self.define(key, values[key]);
-    })
-    return this;
+  function Crystal() {
+    var length = privateValues.push(Object.create(null));
+    this._private_values_ref = length - 1;
   }
 
-  function setter() {
-    throw new Error("Constant '"+ name +"' is already defined.");
+  Crystal.prototype.define = function (name, value) {
+    if (typeof name === 'object') {
+      var self = this, values = name
+      Object.keys(name).forEach(function (key) {
+        self.define(key, values[key]);
+      })
+      return this;
+    }
+
+    if (typeof this[name] !== 'undefined') {
+      throw new Error("Property '"+ name +"' is already defined.")
+    }
+
+    var ref = privateValues[this._private_values_ref]
+
+    function setter(val) {
+      if (hasOwnProperty(ref, name)) {
+        throw new Error("Constant '"+ name +"' is already defined.");
+      }
+      ref[name] = val;
+    }
+
+    Object.defineProperty(this, name, {
+      enumerable: true
+    , configurable: false
+    , set: setter
+    , get: function () { return ref[name]; }
+    });
+
+    return this[name] = value;
+  };
+
+  Crystal.prototype.isDefined = function (name) {
+    var ref = privateValues[this._private_values_ref]
+    return hasOwnProperty(ref, name);
+  };
+
+  Crystal.create = function (values) {
+    var obj = new Crystal()
+    if (typeof values === 'object') {
+      obj.define(values);
+    }
+    return obj;
+  };
+
+  return Crystal;
+};
+
+exports.Crystal = exports.newCrystalConstructor();
+
+
+exports.define = (function () {
+  function makeCrystal(args) {
+    var self = Object.create(null)
+
+    self.on = function (target) {
+      return Crystal.prototype.define.apply(target, args);
+    };
+
+    return self;
   }
 
-  Object.defineProperty(this, name, {
-    value: value
-  , enumerable: true
-  , writable: false
-  , configurable: false
-  , set: setter
-  });
-
-  return value;
-};
-
-Crystal.prototype.isDefined = function (name) {
-  var desc
-  if (desc = Object.getOwnPropertyDescriptor(this, name)) {
-    return desc.writable === false && desc.configurable === false && typeof desc.set === 'function';
+  function define() {
+    return makeCrystal(arguments);
   }
-  return false;
-};
 
-Crystal.create = function (values) {
-  values = values || Object.create(null);
-};
+  return define;
+}());
 
-exports.Crystal = Crystal;
+
+exports.isDefined = (function () {
+  function makeCrystal(args) {
+    var self = Object.create(null)
+
+    self.on = function (target) {
+      return Crystal.prototype.isDefined.apply(target, args);
+    };
+
+    return self;
+  }
+
+  function isDefined() {
+    return makeCrystal(arguments);
+  }
+
+  return isDefined;
+}());
+
+function hasOwnProperty(x, prop) {
+  return Object.prototype.hasOwnProperty.call(x, prop);
+}
--- test/test.js
+++ test/test.js
@@ -1,6 +1,54 @@
-exports['Smoke Test'] = {
-  "It should not be smoking": function (test) {
-    test.ok(true, "It's smoking");
+var Crystal = require('../index').Crystal
+  , define = require('../index').define
+  , isDefined = require('../index').isDefined
+
+exports['When creating a new constant object'] = {
+  setUp: function (done) {
+    this.CONST = Crystal.create();
+    return done();
+  },
+
+  "define() assigns values": function (test) {
+    this.CONST.define('foo', 123);
+    test.strictEqual(this.CONST.foo, 123);
+    return test.done();
+  },
+
+  "defining defined properties raises an error": function (test) {
+    test.expect(1);
+
+    try {
+      this.CONST.define('define', 123);
+    } catch (err) {
+      test.equal(err.message, "Property 'define' is already defined.");
+    }
+
+    return test.done();
+  },
+
+  "defining defined constants raises an error": function (test) {
+    test.expect(1);
+    this.CONST.define('x', 123);
+
+    try {
+      this.CONST.define('x', 456);
+    } catch (err) {
+      test.equal(err.message, "Property 'x' is already defined.");
+    }
+
+    return test.done();
+  },
+
+  "setting defined constants raises an error": function (test) {
+    test.expect(1);
+    this.CONST.define('x', 123);
+
+    try {
+      this.CONST.x = 456;
+    } catch (err) {
+      test.equal(err.message, "Constant 'x' is already defined.");
+    }
+
     return test.done();
   }
-}
+};
